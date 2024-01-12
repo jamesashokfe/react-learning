7cc1928276729675daedb833fc1ae45d9d6d43e8
James Ashok <jamesashok93@gmail.com>
2024-01-12 11:10:44 +0530

    React tutorials initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Message from "./Message";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Message />
+      </div>
+    </>
+  );
+}
+
+export default App;
